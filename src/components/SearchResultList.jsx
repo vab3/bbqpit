5ef0f31b53496022038e64b516d0c556811f6715
Vic Boudolf <victor@roundtrip.com>
2020-07-19 17:07:53 -0400

    format file

--- src/components/SearchResultList.jsx
+++ src/components/SearchResultList.jsx
@@ -1,26 +1,22 @@
-import React from 'react';
-import SearchResult from './SearchResult';
-
+import React from "react";
+import SearchResult from "./SearchResult";
 
 const SearchResultList = (props) => {
-  console.log(`props .items ${props.items}`)
-  if(props.items !== undefined) {
+  console.log(`props .items ${props.items}`);
+  if (props.items !== undefined) {
     const search_results = props.items.map((item) => {
-       return <SearchResult key={item.id} item={item} />
+      return (
+        <SearchResult key={item.id} item={item} />
+      );
     });
+
     return (
       <div>
-        { props.items.length > 0 &&
-          <div>
-            Found {props.items.length} restaurants. 
-          </div>
-        }
-        <div className="ui cards">
-          {search_results}
-        </div>
+        <div>Found {props.items.length} restaurants.</div>
+        <div className="ui cards">{ search_results }</div>
       </div>
-    )
+    );
   }
-}
+};
 
 export default SearchResultList;
